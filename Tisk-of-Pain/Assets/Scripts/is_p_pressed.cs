5442b5142249f65753fa635cc5910f8299b49262
winsor09 <202092@student.asd20.org>
2019-04-29 14:47:37 -0600

    character can move

---
+++ Tisk-of-Pain/Assets/Scripts/is_p_pressed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class is_p_pressed : MonoBehaviour {
+
+    public CameraScript c;
+    public GameObject canvas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            c.enabled = true;
+            canvas.SetActive(false);
+
+        }
+	}
+}
